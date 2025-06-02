fc4b7885bb52f2fc78ef1a927a48a7331a3452c7
Lucky-light-1729 <agrawalanay20@gmail.com>
2025-06-02 10:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const apiKey = "e95eb077ed69a6a387ac5e3bde38b47a";
+const city = "London";
+
+document.getElementById("getWeatherBtn").addEventListener("click", () => {
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    const weatherMain = data.weather[0].main;
+    document.getElementById("weatherData").textContent = `Current weather in London: ${weatherMain}`;
+  })
+  .catch(error => {
+    document.getElementById("weatherData").textContent = `Error fetching weather data: ${error.message}`;
+  });
+});
