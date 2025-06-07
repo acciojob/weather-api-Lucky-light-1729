a165548b74324ae966f424f2c19982b9b12acc15
Anay Agrawal <agrawalanay20@gmail.com>
2025-06-07 09:13:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 const apiKey = "e95eb077ed69a6a387ac5e3bde38b47a";
 const city = "London,uk";
 
-document.getElementById("getWeatherBtn").addEventListener("click", () => {
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-  .then(response => {
-    if (!response.ok) {
+document.getElementById("getWeatherBtn").addEventListener("click", async () => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  
+  try 
+  {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+		        'Accept': 'application/json'
+		      }
+    });
+
+    if (!response.ok) 
+	{
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    return response.json();
-  })
-  .then(data => {
+
+    const data = await response.json();
     const weatherMain = data.weather[0].main;
     document.getElementById("weatherData").textContent = `Current weather in London: ${weatherMain}`;
-  })
-  .catch(error => {
+  } 
+  catch (error) 
+  {
     document.getElementById("weatherData").textContent = `Error fetching weather data: ${error.message}`;
-  });
+  }
 });
-
-
